ed9c5d3752fd5173ecfc6d60c6ba02bdf3466597
Historia Montague <historia.montague@outlook.com>
2023-05-15 12:01:59 -0600

    added python script to add more info to db.json

--- db.py
+++ db.py
@@ -1,15 +1,19 @@
-# import requests
+import requests
 import json
-from operator import itemgetter
-# url = "https://api.thecatapi.com/v1/images/search?limit=50" # replace with the endpoint URL that returns the image
-filename = "test.json" # replace with the filename of the JSON file to modify
 
-# response = requests.get(url)
+url = "https://api.thecatapi.com/v1/images/search?limit=50" # replace with the endpoint URL that returns the image
+filename = "db.json" # replace with the filename of the JSON file to modify
+breeds = ['Orange Tabby']
+
+response = requests.get(url)
 
 with open(filename, 'r') as f:
     json_data = json.load(f)
 
-sortData = sorted(json_data['cats'], key=lambda cat: cat['name'])
+i = 0
+for cat in json_data['cats']:
+    cat['image'] = response[i]['url']
+    i += 1
 
-with open('test.json', 'w') as f:
-    json.dump(sortData, f)
+# with open('test.json', 'w') as f:
+#     json.dump(json_data, f)
