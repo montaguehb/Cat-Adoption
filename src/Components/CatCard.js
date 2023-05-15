e5c65b8573c2ed3c0827c066aef54895c0256243
Meridith Lawn <meridithlawn2@gmail.com>
2023-05-15 13:16:06 -0600

    main components created

---
+++ src/Components/CatCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CatCard() {
+
+
+    
+  return (
+    <div>CatCard</div>
+  )
+}
+
+
+export default CatCard;
---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header"
+import NavBar from "./NavBar"
+import CatCollection from "./CatCollection"
+
+function Home() {
+ 
+
+  return (
+<div>
+<Header/>
+<NavBar/>
+<CatCollection/>
+</div>
+  ); 
+}
+
+export default Home;
