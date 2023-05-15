653bf31b769d70e51ff535d01e982e1b7ae23d4a
Historia Montague <historia.montague@outlook.com>
2023-05-15 16:44:52 -0600

    refactored cats state to be in Home instead of CatCollection

--- src/components/CatCollection.js
+++ src/components/CatCollection.js
@@ -1,18 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import CatCard from "./CatCard"
 
 
-function CatCollection() {
-
-const [cats, setCats] = useState([])
-
-useEffect(() => {
-    fetch("http://localhost:3001/cats")
-    .then(response => response.json())
-    .then(data => setCats(data))
-  },[])
-
+function CatCollection({cats}) {
   const catsArray = cats.map(catObj => <CatCard key={catObj.id} {...catObj}/>)
+  
   return (
     <div>{catsArray}</div>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,17 @@
+import React, {useState, useEffect} from 'react';
 import CatCollection from './CatCollection';
 import CatForm from './CatForm';
 import NavBar from './NavBar';
 
 function Home() {
+
+  const [cats, setCats] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/cats")
+    .then(response => response.json())
+    .then(data => setCats(data))
+  },[])
 
   const addNewCat = async catObj => {
     const resp = await fetch("http://localhost:3000/cats", {
@@ -12,13 +21,14 @@ function Home() {
       },
       body: JSON.stringify(catObj)
     })
-    
+    const data = await resp.json()
+    setCats([...cats, data])
   }
   return (
     <div>
       <CatForm addNewCat={addNewCat}/>
       <NavBar />
-      <CatCollection />
+      <CatCollection cats={cats}/>
     </div>
   );
 }
