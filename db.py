387fa55bf97fb5846ab6ab576458160733fd4d01
Historia Montague <historia.montague@outlook.com>
2023-05-15 16:00:43 -0600

    added script to modify all key/values in db.json and added form

---
+++ db.py
@@ -0,0 +1,12 @@
+import json
+
+filename = "db.json"
+
+with open(filename, 'r') as f:
+    json_data = json.load(f)
+
+for cat in json_data['cats']:
+    cat['adopted'] = 'false'
+
+with open(filename, 'w') as f:
+    json.dump(json_data, f)
--- src/components/CatForm.js
+++ src/components/CatForm.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
-
-function CatForm() {
+function CatForm(props) {
   const [form, setForm] = useState({
     name: "",
     age: "",
@@ -11,14 +10,25 @@ function CatForm() {
   })
 
   const handleChange = ({target: {id, value}}) => setForm({...form, [id]: value})
+  const handleSubmit = e => {
+    e.preventDefault()
+    props.addNewCat(form)
+  }
   return (
-    <form>
-      <input value={form.name} type={"text"} name={"name"} id={"name"} onChange={handleChange}></input>
-      <input value={form.age} type={"text"} name={"age"} id={"age"} onChange={handleChange}></input>
-      <input value={form.breed} type={"text"} name={"breed"} id={"breed"} onChange={handleChange}></input>
-      <input value={form.cost} type={"text"} name={"cost"} id={"cost"} onChange={handleChange}></input>
-      <input value={form.description} type={"text"} name={"description"} id={"description"} onChange={handleChange}></input>
-      <input value={form.image} type={"text"} name={"image"} id={"image"} onChange={handleChange}></input>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name: </label>
+      <input value={form.name} type="text" name="name" id="name" onChange={handleChange}></input>
+      <label htmlFor="age">Age: </label>
+      <input value={form.age} type="text" name="age" id="age" onChange={handleChange}></input>
+      <label htmlFor="breed">Breed: </label>
+      <input value={form.breed} type="text" name="breed" id="breed" onChange={handleChange}></input>
+      <label htmlFor="cost">Suggested Donation: </label>
+      <input value={form.cost} type="text" name="cost" id="cost" onChange={handleChange}></input>
+      <label htmlFor="description">Description: </label>
+      <input value={form.description} type="text" name="description" id="description" onChange={handleChange}></input>
+      <label htmlFor="image">Image: </label>
+      <input value={form.image} type="text" name="image" id="image" onChange={handleChange}></input>
+      <input type="submit" value="Add new cat"></input>
     </form>
   )
 }
