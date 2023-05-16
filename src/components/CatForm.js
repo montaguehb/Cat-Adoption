8b0052aaf09cd7d176c55683b625bab9ec2d1330
Historia Montague <historia.montague@outlook.com>
2023-05-16 13:58:20 -0600

    fixed some bugs

--- src/components/CatForm.js
+++ src/components/CatForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Form, Button} from 'react-bootstrap'
 function CatForm(props) {
   const [form, setForm] = useState({
@@ -9,22 +9,23 @@ function CatForm(props) {
     description: "",
     image: ""
   })
-
-  const handleChange = ({target: {id, value}}) => setForm({...form, [id]: value})
+  const [formArr, setFormArr] = useState([])
+  
   const handleSubmit = e => {
     e.preventDefault()
     props.addNewCat(form)
   }
 
-  const formArr = []
-  for(let key in form) {
-    const newForm = (<Form.Group className='mb-3' controlId='formName' key={key}>
-    {key === "cost" ?<Form.Label>Suggested Donation: </Form.Label>:<Form.Label>{[key]}: </Form.Label>}
-    <Form.Control onChange={handleChange} value={form[key]}></Form.Control>
-    </Form.Group>)
-
-    formArr.push(newForm)
-  }
+  useEffect(()=>{
+    const handleChange = ({target: {id, value}}) => setForm({...form, [id]: value})
+    for(let key in form) {
+      const newForm = (<Form.Group className='mb-3' controlId={`form${key}`} key={key}>
+      {key === "cost" ?<Form.Label>Suggested Donation: </Form.Label>:<Form.Label>{[key]}: </Form.Label>}
+      <Form.Control onChange={handleChange} value={form[key]}></Form.Control>
+      </Form.Group>)
+      setFormArr([...formArr, newForm])
+    }
+  }, [])
 
   return (
     <Form onSubmit={handleSubmit}>
