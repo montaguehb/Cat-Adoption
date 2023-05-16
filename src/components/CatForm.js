dbabb47b72d24b8f52b2ac1cc7d3e0e049406331
Historia Montague <historia.montague@outlook.com>
2023-05-16 11:57:53 -0600

    refactored form with basic styling

--- src/components/CatForm.js
+++ src/components/CatForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {Form, Button} from 'react-bootstrap'
 function CatForm(props) {
   const [form, setForm] = useState({
     name: "",
@@ -14,22 +15,22 @@ function CatForm(props) {
     e.preventDefault()
     props.addNewCat(form)
   }
+
+  const formArr = []
+  for(let key in form) {
+    const newForm = (<Form.Group className='mb-3' controlId='formName' key={key}>
+    {key === "cost" ?<Form.Label>Suggested Donation: </Form.Label>:<Form.Label>{[key]}: </Form.Label>}
+    <Form.Control onChange={handleChange} value={form[key]}></Form.Control>
+    </Form.Group>)
+
+    formArr.push(newForm)
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name: </label>
-      <input value={form.name} type="text" name="name" id="name" onChange={handleChange}></input>
-      <label htmlFor="age">Age: </label>
-      <input value={form.age} type="text" name="age" id="age" onChange={handleChange}></input>
-      <label htmlFor="breed">Breed: </label>
-      <input value={form.breed} type="text" name="breed" id="breed" onChange={handleChange}></input>
-      <label htmlFor="cost">Suggested Donation: </label>
-      <input value={form.cost} type="text" name="cost" id="cost" onChange={handleChange}></input>
-      <label htmlFor="description">Description: </label>
-      <input value={form.description} type="text" name="description" id="description" onChange={handleChange}></input>
-      <label htmlFor="image">Image: </label>
-      <input value={form.image} type="text" name="image" id="image" onChange={handleChange}></input>
-      <input type="submit" value="Add new cat"></input>
-    </form>
+    <Form onSubmit={handleSubmit}>
+      {formArr}
+      <Button variant="primary" type='submit' onSubmit={handleSubmit}>Add New Cat</Button>
+    </Form>
   )
 }
 
